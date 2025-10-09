d181a398bcd60929dfa9c90fe90da8d82829bcf8
TarunBits <tarunbhattmortal@gmail.com>
2025-10-09 20:57:10 +0530

    Initial commit

---
+++ backend/src/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { authRoutes } from "./routes/auth.route.js";
+import { messageRoutes } from "./routes/message.route.js";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+app.listen(PORT, () => console.log("Server running on port: " + PORT));
