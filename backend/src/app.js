c655798c6ea96b1d0123493431b9b47312e78979
TarunBits <tarunbhattmortal@gmail.com>
2025-10-10 16:19:54 +0530

    signup controller added

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,11 +2,14 @@ import express from "express";
 import { authRoutes } from "./routes/auth.route.js";
 import { messageRoutes } from "./routes/message.route.js";
 import path from "path";
+import { connectDB } from "../lib/db.js";
 
 const app = express();
 const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json()); // req.body
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
@@ -20,4 +23,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(PORT, () => console.log("Server running on port: " + PORT));
+app.listen(PORT, () => {
+  console.log("Server running on port: " + PORT);
+  connectDB();
+});
