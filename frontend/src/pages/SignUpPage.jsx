e69d10d31a623e11a35946185809426ebfc167f4
TarunBits <tarunbhattmortal@gmail.com>
2025-10-14 10:12:41 +0530

    frontend codebase setup

---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useAuthStore } from "../store/useAuthStore";
+
+function SignUpPage() {
+  const { authUser, isLoading, login } = useAuthStore();
+  return <div>SignUpPage</div>;
+}
+
+export default SignUpPage;
